8fd72292a78bc537b05e9c2af25708e4473ca9b2
Gabriel <loveragabriel20@gmail.com>
2023-07-16 21:36:58 -0300

    add msg to submitalert

--- src/components/SubmitAlert.js
+++ src/components/SubmitAlert.js
@@ -4,9 +4,9 @@ import Stack from '@mui/material/Stack';
 
 export default function SubmitAlert() {
   return (
-    <Stack sx={{ width: '30%', position: 'fixed', top: '1em' }} spacing={2}>
+    <Stack sx={{ width: '50%', position: 'fixed', top: '1em' }} spacing={2}>
       <Alert variant="filled" severity="success">
-        This is a success alert — check it out!
+        Consulta enviada con éxito!
       </Alert>
     </Stack>
   );
